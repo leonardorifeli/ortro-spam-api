5098189075a1a9be2e02d03fb1aa41ed2e6931f1
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-25 19:25:48 -0300

    Created a client enum and service

---
+++ src/CoreBundle/Business/Enum/ClientEnum.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leonardorifeli
+ * Date: 9/25/16
+ * Time: 7:01 PM
+ */
---
+++ src/CoreBundle/Business/Service/ClientService.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leonardorifeli
+ * Date: 9/25/16
+ * Time: 6:56 PM
+ */
