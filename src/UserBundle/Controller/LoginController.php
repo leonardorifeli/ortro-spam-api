52cdd11aafa9bccca06c736eecac5f5f52b96406
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-10-01 21:01:10 -0300

    Fixed user login API

--- src/UserBundle/Controller/LoginController.php
+++ src/UserBundle/Controller/LoginController.php
@@ -5,13 +5,20 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Business\Service\UserService;
+use CoreBundle\Business\Service\CredentialService;
 
 class LoginController extends Controller
 {
 
-    private function getUserService()
+    private function getUserService() : UserService
     {
         return $this->get('user.service');
+    }
+
+    private function getCredentialService() : CredentialService
+    {
+        return $this->get('core.credential.service');
     }
 
     public function loginAction(Request $request) : Response
@@ -19,8 +26,8 @@ class LoginController extends Controller
         try {
             $user = $this->getUserService()->getUserByEmailAndPasswordToRequest(json_decode($request->getContent()));
 
-            if(is_string($user))
-                return $this->getResponse(['authorized' => false, 'url' => $user, "finded" => !!($user)], 200);
+            if(is_string($user) && $user == "inauthorized")
+                return $this->getResponse(['authorized' => false, 'url' => $this->getCredentialService()->getAuthUrl(), "finded" => !!($user)], 200);
 
             return $this->getResponse(['authorized' => true, 'user' => $user, "finded" => !!($user)], 200);
         } catch (\Exception $e) {
