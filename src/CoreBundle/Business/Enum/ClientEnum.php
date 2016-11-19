10eab9341b9cb75cefbaafd4f936c7ca9d9d74a6
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-11-19 01:07:07 -0200

    Fixed enum formater

--- src/CoreBundle/Business/Enum/ClientEnum.php
+++ src/CoreBundle/Business/Enum/ClientEnum.php
@@ -4,9 +4,7 @@ namespace CoreBundle\Business\Enum;
 
 abstract class ClientEnum
 {
-
     const APPLICATION_NAME = "Gmail API PHP Quickstart";
     const CREDENTIALS_PATH = "~/.credentials/gmail-php-quickstart.json";
     const CLIENT_SECRET_PATH =  __DIR__ . "/../../../../client_secret.json";
-
 }
